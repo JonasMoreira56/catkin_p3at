db353ecf124608aab40bbcb9ae39da52999b2ccb
Jonas Moreira <jonasmoreira076@gmail.com>
2025-02-25 13:35:36 -0300

    init workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jonas/catkin_p3at/src
+# Build directory: /home/jonas/catkin_p3at/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jonas/catkin_p3at/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/jonas/catkin_p3at/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/jonas/catkin_p3at/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/jonas/catkin_p3at/src:$ROS_PACKAGE_PATH"
